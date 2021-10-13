73ead1c108044b91fdf3ea61f0813cdbaa25abf6
prof <prof@KAT12>
2021-10-13 18:46:57 +0200

    Add project files.

---
+++ Inicijali/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Inicijali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ime: ");
+            var ime = Console.ReadLine();
+            Console.Write("Prezime: ");
+            var prezime = Console.ReadLine();
+            if(string.IsNullOrEmpty(ime) || string.IsNullOrEmpty(prezime))
+            {
+                Console.WriteLine("Niste upisali ime ili prezime");
+                return;
+            }
+            Console.WriteLine("Vaši inicijali su: {0}.{1}.", ime[0], prezime.Substring(0, 1));
+        }
+    }
+}
---
+++ TocnoVrijeme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TocnoVrijeme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobar dan. Točno vrijeme je {0}", DateTime.Now);
+        }
+    }
+}
