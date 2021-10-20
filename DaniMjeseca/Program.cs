a9d6f7410b6e3c639a588c528cab9cd7348b3248
tvbegovic <tvrtkobegovic@gmail.com>
2021-10-20 18:43:47 +0200

    Naredbe odluke

---
+++ DaniMjeseca/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DaniMjeseca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite redni broj mjeseca (1-12): ");
+            var unos = Console.ReadLine();
+            var ok = int.TryParse(unos, out int redniBroj);
+            if(!ok)
+            {
+                Console.WriteLine("Pogrešan format");
+                return;
+            }
+            if(redniBroj < 1 || redniBroj > 12)
+            {
+                Console.WriteLine("Broj nije u rasponu 1-12");
+                return;
+            }
+            int brojDana;
+            var sufix = "a";
+            switch(redniBroj)
+            {
+                case 2:
+                    if(DateTime.IsLeapYear(DateTime.Now.Year))
+                    {
+                        brojDana = 29;
+                    }
+                    else
+                    {
+                        brojDana = 28;
+                    }             
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    brojDana = 30;
+                    break;
+                default:
+                    brojDana = 31;
+                    sufix = string.Empty;
+                    break;
+            }
+            Console.WriteLine("{0}. mjesec ima {1} dan{2}", redniBroj, brojDana, sufix);
+        }
+    }
+}
---
+++ DatumiRodenja/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DatumiRodenja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Datum 1: ");
+            var unos = Console.ReadLine();
+            var ok = DateTime.TryParse(unos, out DateTime datum1);
+            if(!ok)
+            {
+                Console.WriteLine("Pogrešan format");
+                return;
+            }
+            Console.Write("Datum 2: ");
+            unos = Console.ReadLine();
+            ok = DateTime.TryParse(unos, out DateTime datum2);
+            if (!ok)
+            {
+                Console.WriteLine("Pogrešan format");
+                return;
+            }
+            if(datum1 < datum2)
+            {
+                Console.WriteLine("Osoba 1 je starija od osobe 2 za {0} dana",
+                    (datum2 - datum1).Days);
+            }
+            else if(datum2 < datum1)
+            {
+                Console.WriteLine("Osoba 2 je starija od osobe 1 za {0} dana",
+                    (datum1 - datum2).Days);
+            }
+            else
+            {
+                Console.WriteLine("Obje osobe su rođene istog datuma");
+            }
+
+            if(datum1.DayOfYear < datum2.DayOfYear)
+            {
+                Console.WriteLine("Osoba 1 slavi rođendan prije osobe 2");
+            }
+            else if(datum2.DayOfYear < datum1.DayOfYear)
+            {
+                Console.WriteLine("Osoba 2 slavi rođendan prije osobe 1");
+            }
+            else
+            {
+                Console.WriteLine("Osobe slave rođendan na isti dan");
+            }
+        }
+    }
+}
