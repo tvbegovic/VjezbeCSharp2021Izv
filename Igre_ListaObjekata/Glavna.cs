6204466368669eca46840f178859dccf828d819e
tvbegovic <tvrtkobegovic@gmail.com>
2021-11-24 18:44:24 +0100

    liste objekata

--- Igre_ListaObjekata/Glavna.cs
+++ Igre_ListaObjekata/Glavna.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
-namespace IgraKlaseNetFramework
+namespace Igre_ListaObjekata
 {
     public partial class Glavna : Form
     {
+        List<Igra> igre = new List<Igra>();
         public Glavna()
         {
             InitializeComponent();
@@ -26,10 +28,10 @@ namespace IgraKlaseNetFramework
 
             var unos = txtDatum.Text;
             DateTime datum = DateTime.Now;
-            if(!string.IsNullOrEmpty(unos))
+            if (!string.IsNullOrEmpty(unos))
             {
                 var ok = DateTime.TryParse(unos, out datum);
-                if(!ok)
+                if (!ok)
                 {
                     MessageBox.Show("Pogrešan format datuma");
                     return;
@@ -37,7 +39,7 @@ namespace IgraKlaseNetFramework
             }
             unos = txtCijena.Text;
             decimal cijena = 0;
-            if(!string.IsNullOrEmpty(unos))
+            if (!string.IsNullOrEmpty(unos))
             {
                 var ok = decimal.TryParse(unos, out cijena);
                 if (!ok)
@@ -55,13 +57,53 @@ namespace IgraKlaseNetFramework
                 igra.DatumIzdavanja = datum;
                 igra.Opis = opis;
                 igra.Izdavac = izdavac;
-                MessageBox.Show("Podaci su uspješno uneseni");
+                igre.Add(igra);
+                AzurirajGrid();
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Dogodila se pogreška. Tekst: {ex.Message}");
             }
-            
+        }
+
+        void AzurirajGrid()
+        {
+            dgvIgre.DataSource = null;
+            dgvIgre.DataSource = igre;
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            var datoteka = new StreamWriter("igre.txt");
+            foreach (var igra in igre)
+            {
+                datoteka.WriteLine("{0};{1};{2};{3};{4};{5}", igra.Naziv, igra.Opis,
+                    igra.Vrsta, igra.DatumIzdavanja, igra.Cijena, igra.Izdavac);
+            }
+            datoteka.Close();
+        }
+
+        private void btnUcitaj_Click(object sender, EventArgs e)
+        {
+            igre.Clear();
+            var redci = File.ReadAllLines("igre.txt");
+            foreach (var red in redci)
+            {
+                var stupci = red.Split(';');
+                var igra = new Igra();
+                igra.Naziv = stupci[0];
+                igra.Opis = stupci[1];
+                igra.Vrsta = stupci[2];
+                var ok = DateTime.TryParse(stupci[3], out DateTime datum);
+                if (ok)
+                    igra.DatumIzdavanja = datum;
+                ok = decimal.TryParse(stupci[4], out decimal cijena);
+                if (ok)
+                    igra.Cijena = cijena;
+                igra.Izdavac = stupci[5];
+                igre.Add(igra);
+            }
+            AzurirajGrid();
         }
     }
 }
